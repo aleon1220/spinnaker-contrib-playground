b944571b08cc146a18441c6811610db1ef61cd5f
Andres LeonRangel <aleon1220@outlook.co.nz>
2026-05-08 17:36:07 +1200

    pulumi AKS feat: add pulumi config for existing resource group deployment

--- kubernetes-azure-spinnaker/src/main/java/myproject/App.java
+++ kubernetes-azure-spinnaker/src/main/java/myproject/App.java
@@ -1,20 +1,23 @@
 package myproject;
 
 import com.pulumi.Pulumi;
+
 import com.pulumi.azurenative.containerservice.ManagedCluster;
 import com.pulumi.azurenative.containerservice.ManagedClusterArgs;
 import com.pulumi.azurenative.containerservice.inputs.ManagedClusterAgentPoolProfileArgs;
 import com.pulumi.azurenative.containerservice.inputs.ManagedClusterIdentityArgs;
 import com.pulumi.azurenative.containerservice.enums.ResourceIdentityType;
-import com.pulumi.azurenative.resources.ResourceGroup;
 
 public class App {
     public static void main(String[] args) {
         Pulumi.run(ctx -> {
-            var resourceGroup = new ResourceGroup("resourceGroup");
+            var config = ctx.config();
+            var location = config.get("azure-native:location").orElse("eastus");
+            var resourceGroup = config.get("resourceGroupName").orElse("rg-tmp-spinnaker");
 
             var cluster = new ManagedCluster("aksCluster", ManagedClusterArgs.builder()
-                    .resourceGroupName(resourceGroup.name())
+                    .resourceGroupName(resourceGroup)
+                    .location(location)
                     .dnsPrefix("aks-dns-prefix")
                     .agentPoolProfiles(ManagedClusterAgentPoolProfileArgs.builder()
                             .name("agentpool")
