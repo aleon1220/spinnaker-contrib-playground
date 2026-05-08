b50b0343fd2a8532958da8c6c47905529dcd5f61
Andres LeonRangel <aleon1220@gmail.com>
2026-05-08 12:29:26 +1200

    pulumi refactor and streamline to goal directories
    1. Refactor the pulumi directory to kubernetes-azure-csharp
    2. Pulumi Java project for kubernetes-azure-spinnaker
    
    Signed-off-by: Andres LeonRangel <aleon1220@gmail.com>

---
+++ kubernetes-azure-spinnaker/src/main/java/myproject/App.java
@@ -0,0 +1,27 @@
+package myproject;
+
+import com.pulumi.Pulumi;
+import com.pulumi.azurenative.resources.ResourceGroup;
+import com.pulumi.azurenative.storage.StorageAccount;
+import com.pulumi.azurenative.storage.StorageAccountArgs;
+import com.pulumi.azurenative.storage.enums.Kind;
+import com.pulumi.azurenative.storage.enums.SkuName;
+import com.pulumi.azurenative.storage.inputs.SkuArgs;
+import com.pulumi.core.Either;
+
+public class App {
+    public static void main(String[] args) {
+        Pulumi.run(ctx -> {
+            var resourceGroup = new ResourceGroup("resourceGroup");
+            var storageAccount = new StorageAccount("sa", StorageAccountArgs.builder()
+                    .resourceGroupName(resourceGroup.name())
+                    .sku(SkuArgs.builder()
+                            .name(SkuName.Standard_LRS)
+                            .build())
+                    .kind(Kind.StorageV2)
+                    .build());
+
+            ctx.export("storageAccountName", storageAccount.name());
+        });
+    }
+}
