b5381ae15000685d305045883cba9f2cfc75560e
Andres LeonRangel <aleon1220@outlook.co.nz>
2026-05-14 18:54:16 +1200

    gradle setting up versioning scm semVer starting with v

--- kubernetes-azure-spinnaker/build.gradle.kts
+++ kubernetes-azure-spinnaker/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     application
     alias(libs.plugins.axion)
+    alias(libs.plugins.shadow)
 }
 
 java {
@@ -23,4 +24,18 @@ dependencies {
 
 application {
     mainClass.set("myproject.App")
+}
+
+// Configure Axion to look for tags starting with 'v'
+scmVersion {
+    tag {
+        prefix.set("v")
+    }
+}
+
+// Crucial step: Set the Gradle project version to the version Axion discovered
+version = scmVersion.version
+
+tasks.shadowJar {
+    isZip64 = true
 }
