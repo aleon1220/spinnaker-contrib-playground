ab28792f34c4af34a92ae939f8ed832e39d2f2b7
Andres LeonRangel <aleon1220@outlook.co.nz>
2026-05-27 20:26:44 +1200

    apply new pattern for gradle management
    - manage everything in the toml file

--- kubernetes-azure-spinnaker/build.gradle.kts
+++ kubernetes-azure-spinnaker/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     application
-    alias(libs.plugins.axion)
     alias(libs.plugins.shadow)
 }
 
@@ -15,29 +14,15 @@ repositories {
 }
 
 dependencies {
-    // Source: https://mvnrepository.com/artifact/com.pulumi/pulumi
-    implementation("com.pulumi:pulumi:1.26.0")
-
-    // Source: https://mvnrepository.com/artifact/com.pulumi/azure-native
-    implementation("com.pulumi:azure-native:3.17.0")
+    implementation(libs.pulumiJava)
+    implementation(libs.azureNative)
 }
 
 application {
     mainClass.set("myproject.App")
 }
 
-// Configure Axion to look for tags starting with 'v'
-scmVersion {
-    tag {
-        prefix.set("v")
-    }
-}
-
-// Crucial step: Set the Gradle project version to the version Axion discovered
-version = scmVersion.version
-
 tasks.shadowJar {
     isZip64 = true
-
     minimize() // todo: test if outcome is a smaller jar
 }
